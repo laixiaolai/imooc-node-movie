911ff43a0f2341eb1a09b31f48e3802d5d56aa69
laixiaolai <laiyongzhao@gmail.com>
2017-07-14 17:54:53 +0800

    卡在mongoDB

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 var express=require('express');
 var bodyParser=require('body-parser');
 var path=require('path');
+var mongoose=require('mongoose');
+var _=require('underscore');
+var Movie=require('./models/movie');
 var port=process.env.PORT||3000;
 var app=express();
+
+mongoose.Promise = global.Promise;  
+mongoose.connect('mongodb:localhost/imooc-node-movie',{useMongoClient:true})
+
 
 app.set('views','./views/pages');
 app.set('view engine','pug');
@@ -14,37 +21,25 @@ console.log('imooc started on port '+port);
 
 //index page
 app.get('/',function(req,res){
-	res.render('index',{
-		title:'imooc 首页',
-	    movies: [{
-	      title: '灌篮高手',
-	      _id: 1,
-	      poster: '/static/bootstrap/dist/fonts/zbrz.jpg'
-	    },{
-	      title: '灌篮高手',
-	      _id: 2,
-	      poster: '/static/bootstrap/dist/fonts/zbrz.jpg'
-	    },{
-	      title: '灌篮高手',
-	      _id: 3,
-	      poster: '/static/bootstrap/dist/fonts/zbrz.jpg'
-	    },{
-	      title: '灌篮高手',
-	      _id: 4,
-	      poster: '/static/bootstrap/dist/fonts/zbrz.jpg'
-	    },{
-	      title: '灌篮高手',
-	      _id: 5,
-	      poster: '/static/bootstrap/dist/fonts/zbrz.jpg'
-	    },{
-	      title: '灌篮高手',
-	      _id: 6,
-	      poster: '/static/bootstrap/dist/fonts/zbrz.jpg'
-	    }]
+	Movie.fetch(function(err,movies){
+		if(err){
+			console.log(err)
+		}
+		res.render('index',{
+			title:'imooc 首页',
+		    movies: movies
+		})
 	})
 })
 //detail page
 app.get('/movie/:id',function(req,res){
+	var id=req.params.id;
+	Movie.findById(id,function(err,movie){
+		res.render('index',{
+			title:'imooc '+movie.title,
+		    movies: movies
+		})
+	});
 	res.render('detail',{
 		title:'imooc 详情页',
 	    movie: {
@@ -75,18 +70,72 @@ app.get('/admin/movie',function(req,res){
 	    }
 	})
 })
+//admin update movie
+app.get('/admin/update/:id',function(req,res){
+	var id=req.params.id;
+
+	if(id){
+		movie.findById(id,function(err,movie){
+			res.render('admin',{
+				title:'imooc 后台更新页面',
+				movie:movie
+			})
+		})
+	}
+})
+
+//admin post movie
+app.post('/admin/movie/new',function(res,req){
+	var id=req.body.movie._id;
+	var movieObj=req.body.movie;
+	var _movie;
+
+	if(id!=='undefined'){
+		Movie.findById(id,function(err,movie){
+			if(err){
+				console.log(err);
+			}
+
+			_movie=_.extend(movie,movieObj)
+			_movie.save(function(err,movie){
+				if(err){
+					console.log(err);
+				}
+				res.redirect('/movie/'+movie._id)
+
+			})
+		})
+	}
+	else{
+		_movie=new Movie({
+			doctor:movieObj.doctor,
+			title:movieObj.title,
+			country:movieObj.country,
+			language:movieObj.language,
+			year:movieObj.year,
+			poster:movieObj.poster,
+			summary:movieObj.summary,
+			flash:movieObj.flash
+		})
+		_movie.save(function(err,movie){
+			if(err){
+				console.log(err);
+			}
+			res.redirect('/movie/'+movie._id)
+		})
+	}
+});
+
 //list page
 app.get('/admin/list',function(req,res){
-	res.render('list',{
-		title:'imooc 列表页',
-	    movies: [{
-	      title: '灌篮高手',
-	      _id: 1,
-	      doctor: '井上雄彦',
-	      country: '美国',
-	      year: 2014,
-	      language: '英语',
-	      flash: 'http://player.youku.com/player.php/sid/XNjA1Njc0NTUy/v.swf'
-	    }]
+
+	Movie.fetch(function(err,movies){
+		if(err){
+			console.log(err)
+		}
+		res.render('list',{
+			title:'imooc 列表页',
+		    movies: movies
+		})
 	})
 })
